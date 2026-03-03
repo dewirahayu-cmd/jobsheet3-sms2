86db972f03d32096483bc0f51032be3aa2203075
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-03-03 23:47:00 +0700

    first

---
+++ matakuliah08.java
@@ -0,0 +1,13 @@
+public class matakuliah08 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public matakuliah08(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
