c8281918170a5989a113ef75a91e3aa5a4ae1fa8
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-03-04 00:16:55 +0700

    modify 4

--- matakuliah08.java
+++ matakuliah08.java
@@ -7,6 +7,13 @@ public class matakuliah08 {
     public matakuliah08() {
     }
 
+    public matakuliah08(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
     public void tambahData(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
