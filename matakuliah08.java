e04ff064c26f9e7605d6e5fc62fa5b82de5b5f52
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-03-03 23:56:01 +0700

    modify 2

--- matakuliah08.java
+++ matakuliah08.java
@@ -4,10 +4,30 @@ public class matakuliah08 {
     public int sks;
     public int jumlahJam;
 
+    // Constructor 1: tanpa parameter, untuk instansiasi kosong dulu
+    public matakuliah08() {
+    }
+
+    // Constructor 2: berparameter lengkap, semua atribut langsung terisi
     public matakuliah08(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    // Constructor 3: berparameter sebagian, hanya kode dan nama saja
+    public matakuliah08(String kode, String nama) {
+        this.kode = kode;
+        this.nama = nama;
+    }
+
+    // Method untuk menambah / memperbarui data
+    public void tambahData(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+        System.out.println("Data matakuliah berhasil diperbarui.");
     }
 }
